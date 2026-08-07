cb2ccabcd1af98c8d0cf7b9e0c2f56dce4bc2b37
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-07 20:13:45 +0900

    Initial commit

---
+++ python/assign1.py
@@ -0,0 +1,25 @@
+#name=input("please enter your name :")
+#age=input("please enter your age :")
+#print("Hello " + name + "! You are " + age + " years old.")
+#question 2
+'''num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+sum=num1+num2
+avg=(num1+num2)/2
+diff=num1-num2
+prod=num1*num2
+div=num1/num2
+print("sum:",sum,"\navg:",avg,"\ndiff:",diff,"\nprod:",prod,"\ndiv:",div)
+'''
+#question 3
+'''a=int(input('enter an integer:'))
+b=int(input("enter another integer:"))
+c=float(input("enter a floating number:"))
+a=float(a)
+b=float(b)
+c=float(c)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+'''
+
