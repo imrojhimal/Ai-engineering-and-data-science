c11c6452ebec19fac5bcefe28369bc03a827fb81
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-07 21:22:08 +0900

    removing comment from all 10 codes

--- python/assign1.py
+++ python/assign1.py
@@ -1,8 +1,9 @@
-#name=input("please enter your name :")
-#age=input("please enter your age :")
-#print("Hello " + name + "! You are " + age + " years old.")
+#question 1
+name=input("please enter your name :")
+age=input("please enter your age :")
+print("Hello " + name + "! You are " + age + " years old.")
 #question 2
-'''num1=int(input("Enter the first number"))
+num1=int(input("Enter the first number"))
 num2=int(input("Enter the second number"))
 sum=num1+num2
 avg=(num1+num2)/2
@@ -10,9 +11,9 @@ diff=num1-num2
 prod=num1*num2
 div=num1/num2
 print("sum:",sum,"\navg:",avg,"\ndiff:",diff,"\nprod:",prod,"\ndiv:",div)
-'''
+
 #question 3
-'''a=int(input('enter an integer:'))
+a=int(input('enter an integer:'))
 b=int(input("enter another integer:"))
 c=float(input("enter a floating number:"))
 a=float(a)
@@ -21,9 +22,9 @@ c=float(c)
 print(a,type(a))
 print(b,type(b))
 print(c,type(c))
-'''
+
 #question 4
-'''
+
 anum=input("enter a numerical string:")
 cvi=int(anum)
 cvf=float(anum)
@@ -31,13 +32,11 @@ cvs=str(anum)
 print(cvi,type(cvi))
 print(cvf,type(cvf))
 print(cvs,type(cvs))
-'''
-'''
+
 #quesstion evalution to question
 x=10+3*2**2
 print(x)#answer should be 22 according to operator precedence
-'''
-'''
+
 #Question 6 swapping
 ab=5
 bc=9
@@ -46,23 +45,21 @@ temp=ab
 ab=bc
 bc=temp
 print(ab,bc)
-'''
-'''
+
 #question 7 temperature
 ct=input("enter temperature in a celcius scale :")
 ct=float(ct)
 ft=ct*(9/5)+32
 print("temperature in farenheit :",ft)
-'''
+
 #question 8 Area of a circle 
-'''
+
 r=input("Enter the radius of the circle")
 r=float(r)
 pi=3.1416
 Area=pi*r**2
 print(Area)
-'''
-'''
+
 #question calculating standard interest
 pr=input("enter the principal")
 pr=float(pr)
@@ -72,7 +69,7 @@ time=input("Enter the time")
 time=float(time)
 si=pr*rate*time
 print(si)
-'''
+
 #question 10 decimal number
 dec=input("enter a decimal number with floating point")
 dec=float(dec)
