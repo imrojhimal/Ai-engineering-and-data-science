6e73b26f55672d2fd58026400fbba33850971bd3
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-21 16:38:05 +0900

    trying lambda function

---
+++ python/average.py
@@ -0,0 +1,7 @@
+def sum(m,n,p):
+    avg=(m+n+p)/3
+    return avg
+a=int(input("Enter the first number:"))
+b=int(input("Enter the 2nd number:"))
+c=int(input("Enter the 3rd number:"))
+print(sum(a,b,c))
