2b636917682691fce5c64f988764da4b690ec470
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-26 17:57:29 +0900

    applying all the methyods of dictionar

--- python/diction.py
+++ python/diction.py
@@ -1,4 +1,10 @@
 info={
     "name":"Imroj","age":25,"Subjects":["maths","Software"],"id":2026,2:'gf'
 }
-print(info[2])
+print(info["Subjects"])
+print(info.keys())
+print(info.values())
+print(info.items())
+print(info.get("age"))
+info.update({"city":"Feni"})
+print(info)
