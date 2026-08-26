4a337ef87f32560a804dcf1f68f7fde632fb38b3
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-26 18:35:29 +0900

    finding unique items in a list via set

--- python/Assignments/week3p1.py
+++ python/Assignments/week3p1.py
@@ -1,4 +1,7 @@
 info=[("alice",'math'),('bob','sci'),('cat','math'),
       ('tania','sci'),('khushbu','phy'),('nahida','phy')]
 print(info)
-print(info[1][1])
+s=set()
+for val in range(len(info)):
+    s.add(info[val][1])
+print(s)
