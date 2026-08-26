1c3c717b10cb1b69bc82de4095c85ce3b731590f
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-08-26 19:16:01 +0900

    applying problem solving on dictionary

--- python/Assignments/week3p1.py
+++ python/Assignments/week3p1.py
@@ -1,10 +1,18 @@
 info=[("alice",'math'),('bob','sci'),('cat','math'),
-      ('tania','sci'),('khushbu','phy'),('nahida','phy')]
-print(info)
+      ('tania','sci'),('khushbu','phy'),('nahida','phy')
+      ,('tania','eng'),('khushbu','math'),('nahida','sci'),
+      ("alice",'phy'),('bob','eng'),('cat','sci')]
 s=set()
 for val in range(len(info)):
     s.add(info[val][1])
-print(s)
 for name,course in info:
     if(course=='sci'):
         print(name)
+dict={}
+for name,course in info:
+    if(dict.get(name)==None):
+        dict.update({name:set()})
+        dict[name].add(course)
+    else:
+        dict[name].add(course)
+print(dict)
