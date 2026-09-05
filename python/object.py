fe4489648dab8aa8aff300651bc749d901adc5f3
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-09-06 05:42:04 +0900

    giving input of cgpa by instance

--- python/object.py
+++ python/object.py
@@ -1,10 +1,12 @@
 class Student:
-    def __init__(self,name):
+    def __init__(self,name,cgpa):
         self.name=name
-
-s1=Student("himal")
-s2=Student("boss")
-s3=Student("Hasan")
-print(s1.name)
+        self.cgpa=cgpa
+    def get_cgpa(self):
+        return self.cgpa
+s1=Student("himal",9.0)
+s2=Student("boss",2.0)
+s3=Student("Hasan",4.0)
+print(f'{s1.name} has a cgpa of {s1.get_cgpa()}')
 print(s2.name)
 
