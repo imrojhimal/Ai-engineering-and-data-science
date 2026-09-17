74493a195a40712fe8a7cb5c371a8f28be39ab89
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-09-18 00:19:12 +0900

    class methods

--- python/instancemethod.py
+++ python/instancemethod.py
@@ -1,11 +1,13 @@
 class Laptop:
-    storage_type="ssd"
     def __init__(self,RAM,storage):
         self.Ram=RAM
         self.storage=storage
+    @classmethod
+    def getstor(cls):
+        print("ssd")
+        
     def get_info(self):
         print(f"laptop has {self.Ram} of Ram and storage of {self.storage} storage type is {self.storage_type}")
 l1=Laptop("16GB",'512GB')
 l2=Laptop('8GB','500GB')
-l1.get_info()   
-l2.get_info() 
+l1.getstor()
