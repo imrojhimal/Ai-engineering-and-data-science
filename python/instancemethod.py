3a8edbcfc1006aa245a79846690d15cbf727ce73
Imroj Ahasan Himal <imrojhimal@gmail.com>
2026-09-18 00:24:48 +0900

    applying all methods in one class

--- python/instancemethod.py
+++ python/instancemethod.py
@@ -8,6 +8,13 @@ class Laptop:
         
     def get_info(self):
         print(f"laptop has {self.Ram} of Ram and storage of {self.storage} storage type is {self.storage_type}")
+    @staticmethod
+    def calc_discount(price,discount):
+        finalprice=price-((price*discount)/100)
+        return finalprice
+
+
 l1=Laptop("16GB",'512GB')
 l2=Laptop('8GB','500GB')
 l1.getstor()
+print(l1.calc_discount(4000,10))
